95084f43f6313f563383ad8b775d679492ff7b32
Hritik <pypi.pioneer@gmail.com>
2023-07-21 22:47:35 +0530

    dark mode enabled to navbar only, form is still left

--- first-react/my-app/src/components/Navbar.js
+++ first-react/my-app/src/components/Navbar.js
@@ -1,6 +1,6 @@
 // importing required packages
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
 /**
  * This component represent Navigation bar.
@@ -10,10 +10,39 @@ import PropTypes from 'prop-types'
  * @returns {JSX.Element} - A JSX element representing the Navbar.
  */
 export default function Navbar(props) {
+
+    // defining the state of the color
+    const [colorModeStyle, setColorModeStyle] = useState({
+        color: 'black',
+        backgroundColor: 'white',
+    });
+
+    const [btnText, setBtntext] = useState("Dark Mode");
+
+    // writing a function for enabling dark mode
+    const handleColorMode = () => {
+        if (colorModeStyle.color == 'white') {
+            setColorModeStyle({
+                color: 'black',
+                backgroundcolor: 'white',
+            });
+            setBtntext("Dark Mode");
+        }
+        else {
+            setColorModeStyle({
+                color: 'white',
+                backgroundColor: 'black',
+            });
+            setBtntext("Light Mode");
+        }
+    }
+
+
+
     return (
         <>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <a className="navbar-brand" href="/">{props.title}</a>
+            <nav className="navbar navbar-expand-lg" style={colorModeStyle} >
+                <a className="navbar-brand" href="/" style={colorModeStyle}>{props.title}</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
@@ -21,16 +50,13 @@ export default function Navbar(props) {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav mr-auto">
                         <li className="nav-item active">
-                            <a className="nav-link" href="/">{props.first} <span className="sr-only">(current)</span></a>
+                            <a className="nav-link" href="/" style={colorModeStyle}>{props.first}</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="/">{props.second}</a>
+                            <a className="nav-link" href="/" style={colorModeStyle}>{props.second}</a>
                         </li>
                     </ul>
-                    <form className="form-inline my-2 my-lg-0">
-                        <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-                        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                    </form>
+                    <button className="btn btn-outline-success my-2 my-sm-0" onClick={handleColorMode} type="button">{btnText}</button>
                 </div>
             </nav>
         </>
