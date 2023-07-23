73d15b4bec5282ba642e2cdf00fdad28b0f94b35
Hritik <pypi.pioneer@gmail.com>
2023-07-23 14:14:55 +0530

    alert msg added

--- first-react/my-app/src/App.js
+++ first-react/my-app/src/App.js
@@ -1,22 +1,37 @@
 import { useState } from 'react';
 import './App.css';
+import Alert from './components/Alert';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 
 // eslint-disable
 function App() {
 
-  // creating an usestate and function to enable dark mode
-  const [colorMode, setColorMode] = useState('light');
+  const [colorMode, setColorMode] = useState('light');   // to enable dark mode
+  const [alertMsg, setAlertMsg] = useState(null);  // to set alert properties (msg, type)
 
+  // creating a method to show alerts
+  const showAlertMsg = (message, type) => {
+    setAlertMsg({
+      message: message,
+      type: type,
+    });
+    setTimeout( () => {
+      setAlertMsg(null);
+    }, 2000);
+  }
+
+  // this method is used to enable dark mode, when button clicked
   const toggleMode = () => {
     if (colorMode === 'light') {
       setColorMode('dark');
-      // document.body.style.backgroundColor = 'darkslategray';
+      document.body.style.backgroundColor = 'darkslategray';
+      showAlertMsg("Dark Mode enabled", "success")
     }
     else {
       setColorMode('light');
-      // document.body.style.backgroundColor = 'white';
+      document.body.style.backgroundColor = 'white';
+      showAlertMsg("Light Mode enabled", "success")
     }
 
   }
@@ -24,7 +39,8 @@ function App() {
   return (
     <>
       <Navbar title="TextUtils" first="Home" second="About" colorMode={colorMode} toggleMode={toggleMode} />
-      <TextForm colorMode={colorMode} />
+      <Alert alertMsg={alertMsg}/>
+      <TextForm colorMode={colorMode} showAlertMsg={showAlertMsg}/>
     </>
   );
 }
