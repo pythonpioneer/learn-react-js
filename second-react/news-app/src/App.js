cd8ed7b48af4afd15d3dc6a580282783d1fcb0ee
Hritik <pypi.pioneer@gmail.com>
2023-07-26 23:45:45 +0530

    navbar added

--- second-react/news-app/src/App.js
+++ second-react/news-app/src/App.js
@@ -1,12 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 import React, { Component } from 'react';
+import Navbar from './components/Navbar';
 
 export default class App extends Component {
   render() {
     return (
-      <div>App</div>
+      <>
+        <Navbar />
+      </>
     )
   }
 }
