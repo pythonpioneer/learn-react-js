5f41741c70e7d4263b5be2c072cd073a2bcd349a
Hritik <pypi.pioneer@gmail.com>
2023-08-05 23:15:42 +0530

    search box added

--- second-react/news-app/src/components/Navbar.js
+++ second-react/news-app/src/components/Navbar.js
@@ -40,7 +40,11 @@ export class Navbar extends Component {
                                 <Link className="nav-link" onClick={this.props.handleCategory} to="/technology">Technology</Link>
                             </li>
                         </ul>
-                        
+                        <form className="form-inline my-2 my-lg-0">
+                            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+                            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                        </form>
+
                     </div>
                 </nav>
             </>
