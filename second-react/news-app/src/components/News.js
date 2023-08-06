833818c7475476812f6458453c29d2ff711988c3
Hritik <pypi.pioneer@gmail.com>
2023-08-07 02:37:39 +0530

    implementing infinite scroll

--- second-react/news-app/src/components/News.js
+++ second-react/news-app/src/components/News.js
@@ -2,6 +2,7 @@ import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import NewsItem from './NewsItem';
 import Loading from './Loading';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 export class News extends Component {
 
@@ -29,7 +30,7 @@ export class News extends Component {
 
     // fetching data from api and loading spinner, when clicked on next, prev and refresh
     async updateApiData(val) {
-        this.setState({loading: true});  // displaying spinner, before fetching data from api
+        this.setState({ loading: true });  // displaying spinner, before fetching data from api
 
         // fetching data from api
         let url = `https://newsapi.org/v2/top-headlines?q=${this.props.searchText}&country=in&category=${this.props.category.toLowerCase()}&apiKey=40e43d4e18e54bd0acb81ab9cf897760&page=${this.state.page + val}&pageSize=${this.props.pageSize}`;
@@ -61,31 +62,34 @@ export class News extends Component {
     render() {
         return (
             <div className='container my-4'>
-                <h2 className='container ml-4'>Top Headlines - {this.props.category==='general'||this.props.category===""?"Global":this.props.category}</h2>
-
-                {/* displaying spinner when api is fetching */}
-                {this.state.loading && <Loading/>}
-
-                {/* displaying the news on the card using loops */}
-                <div className="row">
-                    {!this.state.loading && this.state.articles?.map((element) => {
-                        return <div className="col-md-4 ml-sd-4" key={element.url}>
-                            <NewsItem title={element.title}
-                                desc={element.description}
-                                imgUrl={element.urlToImage}
-                                newsUrl={element.url}
-                                authorName={element.source.name}
-                                publishedAt={element.publishedAt}
-                            />
-                        </div>
-                    })}
-                </div>
+                <h2 className='container ml-4'>Top Headlines - {this.props.category === 'general' || this.props.category === "" ? "Global" : this.props.category}</h2>
+                <InfiniteScroll
+                    dataLength={this.state?.articles == null ? 0 : this.state?.articles?.length}
+                    next={this.updateApiData(1)}
+                    hasMore={true}
+                    loader={<Loading />}
+                >
+                    {/* displaying the news on the card using loops */}
+                    <div className="row">
+                        {!this.state.loading && this.state.articles?.map((element) => {
+                            return <div className="col-md-4 ml-sd-4" key={element.url}>
+                                <NewsItem title={element.title}
+                                    desc={element.description}
+                                    imgUrl={element.urlToImage}
+                                    newsUrl={element.url}
+                                    authorName={element.source.name}
+                                    publishedAt={element.publishedAt}
+                                />
+                            </div>
+                        })}
+                    </div>
+                </InfiniteScroll>
 
                 {/* buttons to navigate on next and previous articles */}
-                <div className="container d-flex justify-content-between">
+                {/* <div className="container d-flex justify-content-between">
                     <button disabled={this.state.page <= 1} className='btn btn-dark ml-4' onClick={this.handlePrevPageClick} >&larr; Previous</button>
                     <button disabled={Math.ceil(this.parsedData?.totalResults/this.props.pageSize) > this.state.page?false:true} className='btn btn-dark mr-4' onClick={() => {this.handleNextPageClick(); this.updateApiData(0);}} >Next &rarr;</button>
-                </div>
+                </div> */}
             </div>
         )
     }
